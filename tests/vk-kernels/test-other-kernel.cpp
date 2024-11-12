61f6576c463559052a0caaa1a956b9deeda44094
Yanwen Xu <xuyanwen2012@gmail.com>
2024-11-12 11:21:55 -0800

    s

--- tests/vk-kernels/test-other-kernel.cpp
+++ tests/vk-kernels/test-other-kernel.cpp
@@ -1,4 +1,5 @@
 #include <glm/glm.hpp>
+#include <random>
 
 #include "common.hpp"
 #include "gtest/gtest.h"
@@ -21,16 +22,26 @@ INSTANTIATE_TEST_SUITE_P(
     VulkanMortonKernelTest,
     ::testing::Values(InitTestParams{1024, 1, "Small_1Block"},
                       InitTestParams{1024, 2, "Small_2Blocks"},
-                      InitTestParams{1024, 3, "Small_3Blocks"}));
+                      InitTestParams{640 * 480, 1, "Medium_1Block"},
+                      InitTestParams{640 * 480, 2, "Medium_2Blocks"},
+                      InitTestParams{1920 * 1080, 1, "Large_1Block"},
+                      InitTestParams{1920 * 1080, 2, "Large_2Blocks"}));
 
 void VulkanMortonKernelTest::RunMortonTestWithBlocks(int n_points,
                                                      int num_blocks) {
   auto u_points = engine.buffer(n_points * sizeof(glm::vec4));
   auto u_morton_keys = engine.buffer(n_points * sizeof(uint));
 
-  //   u_points->fill(glm::vec4(0.0f));
-  for (int i = 0; i < n_points; ++i) {
-    u_points->span<glm::vec4>()[i] = glm::vec4(i, i, i, i);
+  // prepare random points
+  {
+    std::random_device rd;
+    std::mt19937 gen(seed);
+    std::uniform_real_distribution<> dis(min_coord, min_coord + range);
+
+    for (int i = 0; i < n_points; ++i) {
+      u_points->span<glm::vec4>()[i] =
+          glm::vec4(dis(gen), dis(gen), dis(gen), 1.0f);
+    }
   }
 
   u_morton_keys->zeros();
@@ -57,8 +68,15 @@ void VulkanMortonKernelTest::RunMortonTestWithBlocks(int n_points,
 
   auto morton_keys = u_morton_keys->span<uint32_t>();
 
+  std::ranges::sort(morton_keys);
+
   // check if the morton keys are sorted
   for (size_t i = 1; i < morton_keys.size(); ++i) {
-    EXPECT_LT(morton_keys[i - 1], morton_keys[i]);
+    EXPECT_LE(morton_keys[i - 1], morton_keys[i]);
+  }
+
+  // check not zeros
+  for (int i = 0; i < n_points; ++i) {
+    EXPECT_NE(morton_keys[i], 0);
   }
 }
